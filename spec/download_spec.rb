4c8e6d12aea71d0c594fe54a4c3efec9af54291a
Jeffrey Davenport <jeffreydvp@gmail.com>
2017-01-09 21:27:47 -0700

    download_spec - Added multiple tests - fixed errors

--- spec/download_spec.rb
+++ spec/download_spec.rb
@@ -8,8 +8,17 @@ RSpec.describe Download do
   it "should create tmp dir if it doesn't exist" do
     expect(File.exist?(@loc.download.path)).to eq(true)
   end
-  it 'should create an array of only local links' do
-    expect(@download.links.class).to eq(Array)
+
+  describe '.page_hrefs' do
+    before do
+      @download.page_hrefs
+    end
+    it 'should create an array of only local links' do
+      expect(@download.links.class).to eq(Array)
+    end
+    it 'should store all hrefs' do
+      expect(@download.links[0] !~ /\A[0-9]{13}\.zip\z/).to be(0)
+    end
   end
 
   describe '.download' do
@@ -20,15 +29,6 @@ RSpec.describe Download do
     it 'should save 1000000000000.zip' do
       file = File.join(@loc.download.path, @download.links[0])
       expect(File.exist?(file)).to be(true)
-    end
-  end
-
-  describe '.page_hrefs' do
-    before do
-      @download.page_hrefs
-    end
-    it 'should store all hrefs' do
-      expect(@download.links[0] !~ /\A[0-9]{13}\.zip\z/).to be(0)
     end
   end
 
