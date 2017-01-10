c8a90682a8cd16187497d4028daea42ce0a75124
Jeffrey Davenport <jeffreydvp@gmail.com>
2017-01-09 17:45:36 -0700

    download_spec.rb - added download test

--- spec/download_spec.rb
+++ spec/download_spec.rb
@@ -8,7 +8,13 @@ RSpec.describe Download do
   it "should create tmp dir if it doesn't exist" do
     expect(File.exist?(@loc.download.path)).to eq(true)
   end
-  it 'should create an array of links' do
+  it 'should create an array of only local links' do
     expect(@download.links.class).to eq(Array)
+    expect(@download.links[0]).to be('1483718331541.zip')
+  end
+  describe 'download' do
+    it 'should download 1483718331541.zip'
+      expect(File.exist?(File.join(@loc.download.path, '1483718331541.zip'))).to be(true)
+    end
   end
 end
