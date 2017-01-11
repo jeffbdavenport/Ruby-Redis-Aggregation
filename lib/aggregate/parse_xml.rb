fccc303113976c4d3f46aebc83a3421ed5eb4774
Jeffrey Davenport <jeffreydvp@gmail.com>
2017-01-11 14:35:24 -0700

    Parse xml automatically on initialize

--- lib/aggregate/parse_xml.rb
+++ lib/aggregate/parse_xml.rb
@@ -2,7 +2,9 @@ require 'nokogiri'
 # Downloads files from url
 class ParseXML
   def initialize(args)
-    Arguments.valid? args: args, valid: :xml_data
+    Arguments.valid? args: args, valid: [:file, :xml_data]
     @xml = args[:xml_data]
+    @xml ||= File.read args[:file]
+    @xml = Nokogiri::XML @xml
   end
 end
