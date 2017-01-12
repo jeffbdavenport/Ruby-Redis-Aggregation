a17b337e148732a97101ca7db8b8ac8dd46a23b7
Jeffrey Davenport <jeffreydvp@gmail.com>
2017-01-12 14:13:36 -0700

    Moved redis out of module

--- lib/aggregate.rb
+++ lib/aggregate.rb
@@ -2,14 +2,18 @@ $LOAD_PATH.unshift File.dirname(__FILE__)
 $LOAD_PATH.unshift Dir.pwd
 $LOAD_PATH.unshift File.join(Dir.pwd, 'lib')
 $LOAD_PATH.uniq!
+
+autoload :Redis, 'redis'
+
 # Autoload each class for the module and add default paths to load path
 module Aggregate
   autoload :Arguments, 'aggregate/arguments'
   autoload :Download, 'aggregate/download'
   autoload :FileList, 'aggregate/file_list'
   autoload :Locale, 'aggregate/locale'
-  autoload :ParseXML, 'aggregate/parse_xml'
+  autoload :ManageData, 'aggregate/manage_data'
 
   # Use locale throughout module
   LOC ||= Locale.new file: 'config/locale/locale.yml'
+  CONF ||= Locale.new file: 'config/config.yml'
 end
