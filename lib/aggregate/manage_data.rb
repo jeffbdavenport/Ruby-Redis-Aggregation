81a354a0e966bdbbbe2498bae3cd117369729c8c
Jeffrey Davenport <jeffreydvp@gmail.com>
2017-01-15 03:12:20 -0700

    Added lpush alias, and data= method

--- lib/aggregate/manage_data.rb
+++ lib/aggregate/manage_data.rb
@@ -1,28 +1,37 @@
 require 'digest'
 module Aggregate
-  # Insert data into redis
+  # Manage redis data
   class ManageData
-    attr_reader :data, :md5_sum, :list
+    attr_reader :md5_sum, :list
+    attr_writer :data
     def initialize(args)
-      Arguments.valid? args: args, valid: [:file, :data, :redis, :list]
-      @data = args[:data]
-      @data ||= File.read args[:file]
-      @redis = args[:redis]
-      @md5_sum = Digest::MD5.new.update @data
-      @list = args[:list] || CONF.redis.list
+      Arguments.valid? args: args, valid: [:file, :data, :list, :redis]
+      self.data = args[:data] || File.read(args[:file])
+      @list =  args[:list] || CONF.redis.list
+      @redis = args[:redis] || REDIS
     end
 
     # Add data to redis list
     def add_to_list
       return if exists?
       puts LOC.en.manage_data.added % { list: @list, data: @md5_sum }
-      @redis.lpush(@list, @data)
-      @redis.sadd(md5_list, @md5_sum)
+      @redis.lpush @list, @data
+      sadd
+    end
+
+    # Alias
+    def lpush
+      add_to_list
+    end
+
+    # Add the MD5 sum of the data to redis
+    def sadd
+      @redis.sadd md5_list, @md5_sum
     end
 
     # Check if sum is in set
     def exists?
-      @redis.sismember(md5_list, @md5_sum)
+      @redis.sismember md5_list, @md5_sum
     end
 
     # MD5 redis set name
@@ -32,8 +41,14 @@ module Aggregate
 
     # Remove the data from list and the md5 from the md5 list
     def remove
-      @redis.srem(md5_list, @md5_sum)
-      @redis.lrem(@list, -1, @data)
+      @redis.lrem @list, -1, @data if exists?
+      @redis.srem md5_list, @md5_sum
+    end
+
+    def data=(data)
+      @data = data.to_s
+      raise ':data cannot be empty.' if @data.empty?
+      @md5_sum = Digest::MD5.new.update @data
     end
   end
 end
