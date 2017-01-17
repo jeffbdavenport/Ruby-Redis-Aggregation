7ad0669073c2c883c6892997516f133127924649
Jeffrey Davenport <jeffreydvp@gmail.com>
2017-01-17 05:19:32 -0700

    Added message to kill program, and new TmpFile class

--- lib/aggregate.rb
+++ lib/aggregate.rb
@@ -7,14 +7,23 @@ $LOAD_PATH.uniq!
 
 # Autoload each class for the module and add default paths to load path
 module Aggregate
-  autoload :Arguments, 'aggregate/arguments'
-  autoload :Download, 'aggregate/download'
-  autoload :FileList, 'aggregate/file_list'
-  autoload :Locale, 'aggregate/locale'
+  autoload :TmpFile,    'aggregate/tmp_file'
+  autoload :Arguments,  'aggregate/arguments'
+  autoload :Download,   'aggregate/download'
+  autoload :FileList,   'aggregate/file_list'
+  autoload :Locale,     'aggregate/locale'
   autoload :ManageData, 'aggregate/manage_data'
 
   # Use locale throughout module
-  LOC ||= Locale.new file: 'config/locale/locale.yml'
+  LOC ||= Locale.new file:  'config/locale/locale.yml'
   CONF ||= Locale.new file: 'config/config.yml'
   REDIS ||= Redis.new host: CONF.redis.host, port: CONF.redis.port, db: CONF.redis.db
+  THREAD_MAX ||= 4
+
+  def self.kill
+    @file_list = Aggregate::FileList.new path: CONF.file_list.path
+    @file_list.stop_and_remove_parts
+    puts
+    exit
+  end
 end
