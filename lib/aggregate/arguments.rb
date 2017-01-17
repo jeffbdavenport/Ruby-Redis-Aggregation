cbac899c7a3af77f6c2a50006861ba81258e82f2
Jeffrey Davenport <jeffreydvp@gmail.com>
2017-01-17 05:18:50 -0700

    Changed exception classes to messages, because they are not being caught

--- lib/aggregate/arguments.rb
+++ lib/aggregate/arguments.rb
@@ -3,15 +3,17 @@ module Aggregate
   class Arguments
     # Ensure args is a hash and has correct keys.
     def self.valid?(args)
-      raise MustHaveArgs if args[:args].nil?
-      raise HashOnly, args[:args] unless args[:args].respond_to?(:keys)
+      raise '#valid? requires a hash with the :args key' if args[:args].nil?
+      unless args[:args].respond_to?(:keys)
+        raise "Argument \"#{args[:args]}\", must be in hash form."
+      end
       check_valid_array(args[:args], args[:valid])
     end
 
     def self.check_valid_array(args, valid)
       return true if valid.nil?
       args.each_key do |key|
-        raise InvalidKey, key unless valid_has(valid, key)
+        raise "Hash key \"#{key}\", is not a valid key" unless valid_has(valid, key)
       end
       true
     end
@@ -23,30 +25,5 @@ module Aggregate
       valid.include?(key.to_sym)
     end
     private_class_method :valid_has
-
-    # Arguments must be a Hash
-    class MustHaveArgs < StandardError
-      def initialize
-        super '#valid? requires a hash with the :args key'
-      end
-    end
-
-    # Arguments must be a Hash
-    class HashOnly < StandardError
-      attr_reader :args
-      def initialize(args)
-        @args = args
-        super "Argument \"#{@args}\", must be in hash form."
-      end
-    end
-
-    # Arguments hash must have specific keys
-    class InvalidKey < StandardError
-      attr_reader :key
-      def initialize(key)
-        @key = key
-        super "Hash key \"#{@key}\", is not a valid key"
-      end
-    end
   end
 end
