32b4ae4bd3f8db3a4eebd2cf77a49b058e4ac09f
Jeffrey Davenport <jeffreydvp@gmail.com>
2017-01-25 03:36:02 -0700

    Added RakeHelper, reorganized loads and constants

--- lib/aggregate.rb
+++ lib/aggregate.rb
@@ -1,4 +1,5 @@
-require 'redis'
+autoload :Redis, 'redis'
+autoload :RakeHelper, 'rake_helper'
 
 $LOAD_PATH.unshift File.dirname(__FILE__)
 $LOAD_PATH.unshift Dir.pwd
@@ -7,23 +8,27 @@ $LOAD_PATH.uniq!
 
 # Autoload each class for the module and add default paths to load path
 module Aggregate
-  autoload :TmpFile,    'aggregate/tmp_file'
   autoload :Arguments,  'aggregate/arguments'
   autoload :Download,   'aggregate/download'
-  autoload :FileList,   'aggregate/file_list'
   autoload :Locale,     'aggregate/locale'
   autoload :ManageData, 'aggregate/manage_data'
 
   # Use locale throughout module
-  LOC ||= Locale.new file:  'config/locale/locale.yml'
-  CONF ||= Locale.new file: 'config/config.yml'
-  REDIS ||= Redis.new host: CONF.redis.host, port: CONF.redis.port, db: CONF.redis.db
+  LOC        ||= Locale.new file: 'config/locale/locale.yml'
+  CONF       ||= Locale.new file: 'config/config.yml'
   THREAD_MAX ||= 4
+  REDIS      ||= Redis.new  host: CONF.redis.host, port: CONF.redis.port,
+                            db:   CONF.redis.db
 
+  # Required for signal trap, Cannot call require in signal trap
+  # Not at top of file because constants used in constant aliases
+  require_relative 'aggregate/file_list'
+  require_relative 'aggregate/tmp_file'
+
+  # Stop downloads, remove part files and exit
   def self.kill
-    @file_list = Aggregate::FileList.new path: CONF.file_list.path
-    @file_list.stop_and_remove_parts
-    puts
+    FileList.new.stop_and_remove_parts
+    puts # Add blank line for cleanliness
     exit
   end
 end
