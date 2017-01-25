986053aabd8a34b5271be211b113075ea28710c9
Jeffrey Davenport <jeffreydvp@gmail.com>
2017-01-25 03:37:00 -0700

    Moved messages to locale, change hash arguments to normal arguments

--- lib/aggregate/arguments.rb
+++ lib/aggregate/arguments.rb
@@ -2,18 +2,24 @@ module Aggregate
   # Ensures proper arguments are passed to initialize method of a class
   class Arguments
     # Ensure args is a hash and has correct keys.
-    def self.valid?(args)
-      raise '#valid? requires a hash with the :args key' if args[:args].nil?
-      unless args[:args].respond_to?(:keys)
-        raise "Argument \"#{args[:args]}\", must be in hash form."
-      end
-      check_valid_array(args[:args], args[:valid])
+    def self.valid?(args, *valid)
+      raise LOC.en.arguments.args_key if args.nil?
+      raise format LOC.en.arguments.not_hash, args: args unless args.respond_to?(:keys)
+      check_valid_array(args, valid)
     end
 
+    # Fill arguments with provided conf vals
+    def self.fill(args, from, *with)
+      Array(with).each do |key|
+        args[key.to_sym] ||= from.send(key)
+      end
+    end
+
+    # Ensure arguments are valid according to list provided
     def self.check_valid_array(args, valid)
-      return true if valid.nil?
+      return true if valid.empty?
       args.each_key do |key|
-        raise "Hash key \"#{key}\", is not a valid key" unless valid_has(valid, key)
+        raise format LOC.en.arguments.invalid_key key: key unless valid_has(valid, key)
       end
       true
     end
